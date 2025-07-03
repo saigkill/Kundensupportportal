f693c2b0c043bc2b2c957c820d2c32056be25f72
saigkill <himself@saschamanns.de>
2025-07-03 09:21:19 +0200

    Last enhancements

---
+++ src/Kundensupportportal/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Kundensupportportal.Components.Layout;
+public partial class NaMenu : ComponentBase
+{
+
+}
